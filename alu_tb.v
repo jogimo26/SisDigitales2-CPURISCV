6c1fd0d7ac1bb8af25b60e69718b176cd5153ffe
jogimo26 <jogimo26@gmail.com>
2026-09-14 16:13:35 -0500

    Errores con iverilog arreglados

--- alu_tb.v
+++ alu_tb.v
@@ -1,73 +1,73 @@
 module alu_tb;
 
-reg [7:0] A;
-reg [7:0] B;
-reg [1:0] op;
-wire [7:0] ALU_out;
+reg [31:0] a;
+reg [31:0] b;
+reg [3:0] op;
+wire [31:0] alu_out;
 
-// Instancia de la ALU
-ALU uut (
-    .A(A),
-    .B(B),
+// Instancia de la alu
+alu uut (
+    .a(a),
+    .b(b),
     .op(op),
-    .ALU_out(ALU_out)
+    .alu_out(alu_out)
 );
 
 initial begin
 
     $display("------------------------------------------------");
-    $display(" Tiempo\tA\tB\top\tALU_out");
+    $display(" Tiempo\ta\tb\top\talu_out");
     $display("------------------------------------------------");
 
     $monitor("%4dns\t%d\t%d\t%b\t%d",
-             $time, A, B, op, ALU_out);
+             $time, a, b, op, alu_out);
 
     // Prueba 1: Suma
-    A = 8'd10;
-    B = 8'd50;
-    op = 2'b00;
+    a = 32'd10;
+    b = 32'd50;
+    op = 4'b0000;
     #10;
 
     // Prueba 2: Resta
-    A = 8'd110;
-    B = 8'd19;
-    op = 2'b01;
+    a = 32'd110;
+    b = 32'd19;
+    op = 4'b1000;
     #10;
 
-    // Prueba 3: AND
-    A = 8'b110110;
-    B = 8'b10111011;
-    op = 2'b10;
+    // Prueba 3: aND
+    a = 32'b110110;
+    b = 32'b10111011;
+    op = 4'b0111;
     #10;
 
     // Prueba 4: OR
-    A = 8'b11001100;
-    B = 8'b10101010;
-    op = 2'b11;
+    a = 32'b11001100;
+    b = 32'b10101010;
+    op = 4'b0110;
     #10;
 
     // Prueba 5: Suma
-    A = 8'd15;
-    B = 8'd55;
-    op = 2'b00;
+    a = 32'd15;
+    b = 32'd55;
+    op = 4'b0000;
     #10;
 
     // Prueba 6: Resta (resultado negativo)
-    A = 8'd30;
-    B = 8'd10;
-    op = 2'b01;
+    a = 32'd30;
+    b = 32'd10;
+    op = 4'b1000;
     #10;
 
-    // Prueba 7: AND con todos unos
-    A = 8'hFF;
-    B = 8'h0F;
-    op = 2'b10;
+    // Prueba 7: aND con todos unos
+    a = 32'hFF;
+    b = 32'h0F;
+    op = 4'b0111;
     #10;
 
     // Prueba 8: OR con cero
-    A = 8'h00;
-    B = 8'hAA;
-    op = 2'b11;
+    a = 32'h00;
+    b = 32'haa;
+    op = 4'b0110;
     #10;
 
     $finish;
